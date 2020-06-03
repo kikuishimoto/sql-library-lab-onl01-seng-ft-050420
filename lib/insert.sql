f555b0a05cc1f1b3870ab9e4cf88b474f0eaf861
kiku ishimoto <kikuish@gmail.com>
2020-06-03 23:50:13 +0000

    Automatically backed up by Learn

---
+++ lib/insert.sql
@@ -0,0 +1,3 @@
+INSERT INTO series (id, title, author_id, subgenre_id) VALUES (1, 'Harry Potter', 1 ,1), (2, 'Hunger Games', 2, 2);
+INSERT INTO books (id, title, year, series_id) VALUES (1, 'Prisoner of Azkaban', 1999, 1), (2, 'Chamber of Secrets', 1998, 1),
+(3, 'The Deathly Hallows', 2007, 1), (4, 'Hunger Games', 2008, 2), (5, 'Catching Fire', 2009, 2), (6, 'Mocking Jay', 2010, 2);
