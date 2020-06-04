86823664d547714fb5ec9b3c418702b042ad81ac
kiku ishimoto <kikuish@gmail.com>
2020-06-04 00:20:50 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -8,3 +8,5 @@ INSERT INTO characters (id, name, motto, species, author_id) VALUES (1, 'Harry',
 (8, 'Gale', 'friend zone king','human', 2);
 INSERT INTO subgenres (id, name) VALUES (1, 'fantacy'), (2, 'dystopian');
 INSERT INTO authors(id, name) VALUES (1, 'J.K. Rowling'), (2, 'Suzanne Collins');
+INSERT INTO character_books (id, book_id, character_id) VALUES (1, 1, 1), (2,1,2), (3, 1, 3), (4, 1, 4), (5, 2, 5), (6, 2, 6), (7, 2, 7),
+(8, 2, 8), (9, 4, 5), (10, 4, 6), (11, 5, 6), (12, 6, 6), (13, 4, 7), (14, 5, 7), (15, 6, 7), (16, 4, 8);
